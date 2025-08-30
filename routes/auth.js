8651b5b5c7922a21e35c526480edeb46cdf6d994
jhalakbhandari <82494331+jhalakbhandari@users.noreply.github.com>
2025-08-30 14:37:04 +0530

    added cloudinary for photos

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-
+import authMiddleware from "../middlewares/authentication.js";
 const router = express.Router();
 // const JWT_SECRET = "supersecret"; // ⚠️ use env variable in prod
 
@@ -29,7 +29,7 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email }).populate("room");
-  if (!user) return res.status(404).json({ message: "User not found" });
+  if (!user) return res.status(404).json({ message: "User not found. Incorrect email or password." });
 
   const validPassword = await bcrypt.compare(password, user.password);
   if (!validPassword)
@@ -44,6 +44,19 @@ router.post("/login", async (req, res) => {
     token,
     roomId: user.room ? user.room._id : null, // tell frontend if user already has room
   });
+});
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).populate("room");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json({
+      user,
+      roomId: user.room ? user.room._id : null,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 export default router;
--- routes/post.js
+++ routes/post.js
@@ -3,24 +3,35 @@ import express from "express";
 import Post from "../models/Post.js";
 const router = express.Router();
 import authMiddleware from "../middlewares/authentication.js";
-import multer from "multer";
 import path from "path";
+import cloudinary from "../config/cloudinary.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/"); // folder to save images
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+// const upload = multer({ storage });import cloudinary from "../config/cloudinary.js";
 
-// Multer setup
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // folder to save images
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "myapp_uploads",
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
-const upload = multer({ storage });
 
+const upload = multer({ storage });
 router.post("/", authMiddleware, upload.single("photo"), async (req, res) => {
   try {
     const { roomId, text } = req.body;
-    const photo = req.file ? req.file.filename : "";
+    // const photo = req.file ? req.file.filename : "";
+    const photo = req.file ? req.file.path : "";
+
     if (!text && !photo)
       return res.status(400).json({ message: "Post cannot be empty" });
 
